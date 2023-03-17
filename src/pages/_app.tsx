54f003a66e7a7e3f8812c75536a423e91550db9d
tonyandy5630 <tonyandy456@gmail.com>
2023-03-17 19:54:20 +0700

    set up react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,24 @@ import React from "react"
 import "../styles/global.css"
 import type { AppProps } from "next/app"
 import { AuthContextProvider } from "../context/AuthContext"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  })
+
   return (
-    <AuthContextProvider>
-      <Component {...pageProps} />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   )
 }
