19ca34b44b720a1c2419e9726753a012c7ec9a46
tonyandy5630 <tonyandy456@gmail.com>
2023-03-23 19:03:55 +0700

    add verify page

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,7 +1,11 @@
 import { LoginResponse, RegisterResponse } from "types/auth.type"
+import { ResponseAPI } from "types/utils.type"
 import http from "utils/http"
 
 export const registerAccountAPI = (body: { email: string; username: string; pwd: string }) =>
   http.post<RegisterResponse>("/auth/signup", body)
 
 export const LoginAPI = (body: { username: string; pwd: string }) => http.post<LoginResponse>("/auth/login", body)
+
+export const verifyEmailAPI = (email: string, verifyToken: string) =>
+  http.get<ResponseAPI<{}>>(`auth/verification/${email}/${verifyToken}`)
