57fb5064bd9fa73bbcc445a8711c06acda90cf98
tonyandy5630 <tonyandy456@gmail.com>
2023-04-13 00:56:54 +0700

    update token hook

--- src/hooks/getToken.ts
+++ src/hooks/getToken.ts
@@ -1,5 +1,5 @@
 import { log } from "console"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { checkLoginAPI } from "apis/auth.api"
 import { useEffect, useMemo, useRef } from "react"
 import { User } from "types/user.type"
@@ -8,6 +8,7 @@ export default function useGetToken() {
   const accessToken = useRef<string>("")
   const userToken = useRef<User>({ email: "", username: "", followers: 0, rankInSubjects: [] })
   const queryState = useRef<string>("loading")
+  // const queryClient = useQueryClient()
   const checkLoginQuery = useQuery(["check-login"], { queryFn: checkLoginAPI, retry: 1 })
   const { data, error, status } = checkLoginQuery
 
@@ -19,6 +20,7 @@ export default function useGetToken() {
       if (user) userToken.current = user
       if (access_token) accessToken.current = access_token
     }
+    // if (status === "success") queryClient.invalidateQueries(["check-login"])
     queryState.current = status
   }, [data, status])
 
